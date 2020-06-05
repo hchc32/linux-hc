683c5cd6141551628b64a2e42918e84ee718c124
hchc32 <1844382037@qq.com>
2020-06-05 19:58:13 +0800

    msg_Cs

---
+++ Linux_c/ps/IPC/client_msg.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<fcntl.h>
+#include<string.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<sys/stat.h>
+
+#define BUF_SIZE 256
+#define PROJ_ID 32
+#define PATH_NAME "/tmp"
+//消息队列中消息类型
+#define SERVER_MSG 1
+#define CLIENT_MSG 2
+
+int main()
+{
+    //用户自定义消息缓冲区
+    struct mymsgbuf
+    {
+        long msgtype;
+        char ctrlstring[BUF_SIZE];
+    }msgbuffer;
+    //消息队列标识符
+    int qid;
+    //消息长度
+    int msglen;
+    //键值
+    key_t msgkey;
+
+    //获区键值
+    if((msgkey = ftok(PATH_NAME,PROJ_ID)) == -1)
+    {
+        perror("ftok error!");
+        exit(1);
+    }
+
+    //获取消息队列标识符
+    if((qid = msgget(msgkey,IPC_CREAT | 0666)) == -1)
+    {
+        perror("msgget error!");
+        exit(0);
+    }
+
+    //读写操作
+    while(1)
+    {
+        if(msgrcv(qid,&msgbuffer,BUF_SIZE,SERVER_MSG , 0) == -1)
+        {
+            perror("server msgrcv error!");
+            exit(1);
+        }
+        printf("Server: %s\n",msgbuffer.ctrlstring);
+        printf("Client: ");
+        fgets(msgbuffer.ctrlstring , BUF_SIZE ,stdin);
+        if(strncmp("exit",msgbuffer.ctrlstring,4) == 0)
+        {
+            break;
+        }
+        msgbuffer.ctrlstring[strlen(msgbuffer.ctrlstring)-1] = '\0';
+        msgbuffer.msgtype = CLIENT_MSG;
+        if((msgsnd(qid , &msgbuffer ,strlen(msgbuffer.ctrlstring),0)) == -1)
+        {
+            perror("client msgsnd error!");
+            exit(1);
+        }
+    }
+    return 0;
+}
+
