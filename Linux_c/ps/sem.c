09034a79ff640f4d7193d7e1daaa2ca0d0a9d6a7
hchc32 <1844382037@qq.com>
2020-06-06 08:11:15 +0800

    learn

--- Linux_c/ps/sem.c
+++ Linux_c/ps/sem.c
@@ -7,13 +7,32 @@ int sem_p(int semid , int index)
     struct sembuf buf = {0,-1,IPC_NOWAIT};
     if(index < 0)
     {
-        perror("index ");
+        //数组的索引不能是负值
+        perror("index of array cannot equals a minus value");
         return -1;
     }
     buf.sem_num = index;
     if(semop(semid , &buf ,1) == -1)
     {
-        perror("semop");
+        perror("a wrong operation to semaphore occurred! ");
+        return -1;
+    }
+    return 0;
+}
+
+int sem_v(int semid,int index)
+{
+    struct sembuf buf = {0,1,IPC_NOWAIT};
+    if(index < 0)
+    {
+        perror("index of array cannot equals a minus value");
+        return -1;
+    }
+
+    buf.sem_num = index;
+    if(semop(semid,&buf,1) == -1)
+    {
+        perror("a wrong operation to semaphore occurred! ");
         return -1;
     }
     return 0;
