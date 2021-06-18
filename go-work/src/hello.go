ef31ccdf17cbd8d053e2ab6fa3b2ca1dfe531765
hchc32 <1844382037@qq.com>
2021-06-18 23:19:58 +0800

    learn

---
+++ go-work/src/hello.go
@@ -0,0 +1,12 @@
+//包声明,每个程序必须有一个main包
+package main
+
+//引入包
+import (
+    "fmt"   //格式化io包
+)
+
+func main() {
+    fmt.Println("hello, world")
+    fmt.Printf("Google" + "Runoob")
+}
---
+++ go-work/src/var.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+var _a string
+func main(){
+	//变量名后跟类型
+	var a string = "Runoob"
+	//栈上对象也是０
+	var stack_a string;
+	fmt.Println(stack_a)
+	fmt.Println(a)
+	fmt.Println(_a)
+	var b,c int = 1,2
+	fmt.Println(b,c)
+
+	//默认false
+	var _bool bool;
+	fmt.Println(_bool)
+
+
+}
