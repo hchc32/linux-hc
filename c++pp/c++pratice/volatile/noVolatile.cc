2684a1f2eb1c62724e1dd0e79b4011c344efb50d
hchc32 <1844382037@qq.com>
2021-07-24 15:12:17 +0800

    learn

---
+++ c++pp/c++pratice/volatile/noVolatile.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int fn(int afn)
+{
+    return afn+1;
+}
+
+int main()
+{
+    int a = 5;
+    int b = 10;
+    int c = 20;
+    int d;
+    cin >> c;
+
+    a = fn(c);//1+1
+    b = a + 1;//3
+    d = fn(b);//4
+    cout << a << b << c << d;
+    return 0;
+}
